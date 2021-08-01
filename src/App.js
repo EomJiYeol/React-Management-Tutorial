8a8036e11c31572b698726fe8183f9afb1bbf911
EomJiYeol <wlduf6986@naver.com>
2021-08-01 11:24:18 +0900

    Success Material UI

--- src/App.js
+++ src/App.js
@@ -20,33 +20,7 @@ const styles= theme=>({
     minWidth:1080
   }
 })
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/any1",
-    name: "하두부",
-    birthday: "901204",
-    gender: "여",
-    job: "판사",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/any2",
-    name: "엄수박",
-    birthday: "910317",
-    gender: "남",
-    job: "변호사",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/any3",
-    name: "하푸린",
-    birthday: "920615",
-    gender: "남",
-    job: "의사",
-  },
-];
-=======
+
 const customers = [{
   'id' :1,
   'image' :'https://placeimg.com/64/64/any1',
@@ -72,14 +46,12 @@ const customers = [{
   'job': "의사",
   
 }];
->>>>>>> d315829dc1fe967e810470ab7f1c7e7c0856a053
 class App extends Component {
   render() {
 
     const{classes} = this.props;
     
     return (
-<<<<<<< HEAD
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
@@ -97,27 +69,7 @@ class App extends Component {
           </TableBody>
         </Table>
       </Paper>
-=======
-      <div>
-        {
-          customers.map(c=>{
-            return(
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            )
-          })
-        }
-  </div>
->>>>>>> d315829dc1fe967e810470ab7f1c7e7c0856a053
     );
   }
 }
 export default withStyles(styles)(App);
-/**   */
